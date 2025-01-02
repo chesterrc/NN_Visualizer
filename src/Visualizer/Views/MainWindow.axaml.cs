bff7504155f5c1203db8411310ab619e30d40d19
chesterrc <chezramosc@gmail.com>
2025-01-02 01:45:31 -0500

    Finsihed base avalonia tutorial

--- src/Visualizer/Views/MainWindow.axaml.cs
+++ src/Visualizer/Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Logging;
+using System.Diagnostics;
 
 namespace Visualizer.Views;
 
@@ -7,5 +10,19 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    public void ButtonClicked(object source, RoutedEventArgs args)
+    {    
+        if (double.TryParse(celsius.Text, out double C))
+        {
+            var F = C * (9d / 5d) + 32;
+            fahrenheit.Text = F.ToString("0.0");
+        }
+        else
+        {
+            celsius.Text = "0";
+            fahrenheit.Text = "0";
+    }
+
     }
 }
--- src/Visualizer/obj/Debug/net8.0/Visualizer.AssemblyInfo.cs
+++ src/Visualizer/obj/Debug/net8.0/Visualizer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Visualizer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad6c459ea46d87b1d652740b7d791bd9ddcc0f5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aaf01199ca096b22554a568917c83123d1eedd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Visualizer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Visualizer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
